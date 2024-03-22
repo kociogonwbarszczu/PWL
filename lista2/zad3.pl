855eb4e71b3cedce9d45ea910e8a66dad4669f8f
Kinga <272354@student.pwr.edu.pl>
2024-03-22 17:34:54 +0100

    working exercise 3

---
+++ lista2/zad3.pl
@@ -0,0 +1,23 @@
+%
+%       a <=====> b
+%                 |
+%                 \/
+%       d<--------c
+%
+
+arc(a, b).
+arc(b, a).
+arc(b, c).
+arc(c, d).
+
+reachable(X, X).
+reachable(X, Y) :- reachable(X, Y, [X]).
+
+reachable(X, Y, Visited) :-
+    arc(X, Z),
+    \+ member(Z, Visited),
+    (
+        Y=Z
+        ;
+        reachable(Z, Y, [Z | Visited])
+    ).
