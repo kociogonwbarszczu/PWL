12910a7b6155332e731b07cf429d98b851e21ef1
Kinga <272354@student.pwr.edu.pl>
2024-04-12 21:09:06 +0200

    zad 1 fully working

--- lista4/zad1.pl
+++ lista4/zad1.pl
@@ -1,20 +1,4 @@
-possible_expression([X], X, X).
-possible_expression(List, _, Expression) :-
-    append(List1, List2, List),
-    List1 \= [],
-    List2 \= [],
-    possible_expression(List1, Value1, Expression1),
-    possible_expression(List2, Value2, Expression2),
-    (   Expression1 = Value1,
-        Expression2 = Value2,
-        member(Op, ['+', '-', '*', '/']),
-        Expression = expr(Op, Expression1, Expression2)
-    ;   Value1 \= Expression1,
-        Value2 \= Expression2,
-        member(Op, ['+', '-', '*', '/']),
-        Expression = expr(Op, Expression1, Expression2)
-    ).
-
+% Evaluate arithmetic expressions
 evaluate_expression(expr('+', Left, Right), Value) :-
     evaluate_expression(Left, LeftValue),
     evaluate_expression(Right, RightValue),
@@ -32,19 +16,36 @@ evaluate_expression(expr('/', Left, Right), Value) :-
     evaluate_expression(Right, RightValue),
     RightValue =\= 0,
     Value is LeftValue / RightValue.
-
 evaluate_expression(Number, Number) :- number(Number).
 
+% Normalize expressions
 normalize_expression(expr(Op, Left, Right), NormalExpr) :-
     normalize_expression(Left, LeftExpr),
     normalize_expression(Right, RightExpr),
     atomic_list_concat(['(', LeftExpr, Op, RightExpr, ')'], NormalExpr).
 normalize_expression(Number, Number) :- number(Number).
 
+% Generate possible expressions from a list of numbers
+possible_expression([X], X, X).
+possible_expression(List, _, Expression) :-
+    append(List1, List2, List),
+    List1 \= [],
+    List2 \= [],
+    member(Op, ['+', '-', '*', '/']),
+    possible_expression(List1, Value1, Expression1),
+    possible_expression(List2, Value2, Expression2),
+    (
+        (Expression1 = Value1, Expression2 = Value2);
+        (Value1 \= Expression1, Value2 \= Expression2)
+    ),
+    Expression = expr(Op, Expression1, Expression2).
+
+% Get expressions, evaluate them, and normalize them
 get_expressions(List, Value, Expression, NormalExpression) :-
     possible_expression(List, Value, Expression),
     evaluate_expression(Expression, Value),
     normalize_expression(Expression, NormalExpression).
 
+% Main predicate to get the expression for a given value
 expression(List, Value, Expression) :-
     get_expressions(List, Value, _, Expression).
